eefdefa871204e67bf03d3d71366042eebc9676a
Ivan Dustin Bilon <ivan22.dust@gmail.com>
2024-05-14 02:03:28 +0800

    Remove prefix

--- dagster_pieces/assets/references.py
+++ dagster_pieces/assets/references.py
@@ -1,9 +1,8 @@
 from pandas import DataFrame, read_csv
 from dagster import asset
-from dagster_pieces.constants import PREFIX
 
 
-@asset(key_prefix=PREFIX)
+@asset
 def references() -> DataFrame:
     return read_csv(
         "https://raw.githubusercontent.com"
